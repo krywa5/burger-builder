bffa1046e4fb4dfda3e6065efeb76322e4126f03
Krystian Wasilewski <blocker92@o2.pl>
2019-03-26 12:32:46 +0100

    Updates

---
+++ precache-manifest.7b26b5285b3321b33bdcfa09cf2a34b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "95aae902d443e9981fb8",
+    "url": "/burger-builder/static/js/runtime~main.95aae902.js"
+  },
+  {
+    "revision": "3938ee0973a683f005c8",
+    "url": "/burger-builder/static/js/main.3938ee09.chunk.js"
+  },
+  {
+    "revision": "04cd1961d768f130e758",
+    "url": "/burger-builder/static/js/2.04cd1961.chunk.js"
+  },
+  {
+    "revision": "3938ee0973a683f005c8",
+    "url": "/burger-builder/static/css/main.1c9b6cf3.chunk.css"
+  },
+  {
+    "revision": "0ebe392bb69bdb3769f27da440bfc104",
+    "url": "/burger-builder/index.html"
+  }
+];
