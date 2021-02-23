c310e4687cb5d10a93e84735101bb8dbba3822a5
krywa5 <blocker92@o2.pl>
2021-02-23 20:18:37 +0100

    Improved form validation and created Auth form

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import classes from "./Auth.module.css";
+
+const initControls = {
+  name: {
+    elementType: "input",
+    elementConfig: {
+      type: "email",
+      placeholder: "Mail address",
+    },
+    value: "",
+    validation: {
+      required: true,
+      isEmail: true,
+    },
+    valid: false,
+    touched: false,
+  },
+  password: {
+    elementType: "input",
+    elementConfig: {
+      type: "password",
+      placeholder: "Password",
+    },
+    value: "",
+    validation: {
+      required: true,
+      minLength: 6,
+    },
+    valid: false,
+    touched: false,
+  },
+};
+
+const Auth = () => {
+  const [controls, setControls] = useState(initControls);
+
+  const checkValidity = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  };
+
+  const inputChangeHandler = (event, controlName) => {
+    event.preventDefault();
+
+    const updatedControls = {
+      ...controls,
+      [controlName]: {
+        ...controls[controlName],
+        value: event.target.value,
+        valid: checkValidity(
+          event.target.value,
+          controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+
+    setControls(updatedControls);
+  };
+
+  const formElementsArray = [];
+  for (let key in controls) {
+    formElementsArray.push({
+      id: key,
+      config: controls[key],
+    });
+  }
+
+  const form = formElementsArray.map((formElement) => (
+    <Input
+      key={formElement.id}
+      elementType={formElement.config.elementType}
+      elementConfig={formElement.config.elementConfig}
+      value={formElement.config.value}
+      invalid={!formElement.config.valid}
+      shouldValidate={formElement.config.validation}
+      touched={formElement.config.touched}
+      changed={(event) => inputChangeHandler(event, formElement.id)}
+    />
+  ));
+
+  return (
+    <div className={classes.Auth}>
+      <form>
+        {form}
+        <Button btnType="Success">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -98,6 +98,9 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
@@ -110,6 +113,17 @@ class ContactData extends Component {
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
     }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
     return isValid;
   }
 
