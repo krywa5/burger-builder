679e4ddcafbdb73f60a7e569a67c800657b22e70
krywa5 <blocker92@o2.pl>
2021-02-24 20:42:17 +0100

    code improvement

--- src/store/utility.js
+++ src/store/utility.js
@@ -1,4 +1,10 @@
 export const updateObject = (oldObject, updatedProperties) => {
+  if (typeof oldObject !== "object" || typeof updatedProperties !== "object") {
+    console.error(
+      "Both oldObject and updatedProperties are expected to be objects."
+    );
+    return {};
+  }
   return {
     ...oldObject,
     ...updatedProperties,
